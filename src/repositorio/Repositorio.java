7d581d159f2bad3727dc5e9cdc5c1437f28def31
Johnner Yelcde <johnnercastrojy@gmail.com>
2023-12-04 16:54:00 -0300

    Começando a criar repositório

--- src/repositorio/Repositorio.java
+++ src/repositorio/Repositorio.java
@@ -1,4 +1,21 @@
 package repositorio;
 
 public class Repositorio {
+    private ArrayList<Evento> eventos = new ArrayList<>();
+    private ArrayList<Participante> participantes = new ArrayList<>();
+    private ArrayList<Ingresso> ingressos = new ArrayList<>();
+
+    public void criarEvento(data,descrição,capacidade, preco) {
+        eventos.add(Evento evento = new Evento(String data, String decricao, int capacidade, double preco));
+    }
+    public void apagarEvento(id){
+        eventos.remove(evento);
+    }
+    public Evento localizarEvento(int id) {
+        for(Evento i : eventos)
+            if (evento.getId() == id) {
+                return i;
+            }
+        return null;
+    }
 }
