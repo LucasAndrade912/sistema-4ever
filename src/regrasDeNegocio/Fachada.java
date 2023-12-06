d5b884bd3832b9c5bcbcdbfcf0b4d0fbadb068aa
Lucas Andrade dos Santos <lucasantos.dev@gmail.com>
2023-12-06 00:22:55 -0300

    implementando mais funcionalidades

--- src/regrasDeNegocio/Fachada.java
+++ src/regrasDeNegocio/Fachada.java
@@ -1,5 +1,9 @@
 package regrasDeNegocio;
 
+import modelosDeNegocio.Convidado;
+import modelosDeNegocio.Evento;
+import modelosDeNegocio.Ingresso;
+import modelosDeNegocio.Participante;
 import repositorio.Repositorio;
 
 public class Fachada {
@@ -8,11 +12,46 @@ public class Fachada {
     public static void criarEvento(String data, String descricao, int capacidade, double preco) throws Exception {
         if (preco < 0)
             throw new Exception("Preço inválido!! Coloque um preço maior ou igual a zero.");
-        else if (data == null || descricao == null)
+        if (data.isEmpty() || descricao.isEmpty())
             throw new Exception("A data e a descrição são obrigatórias!!");
-        else if (capacidade < 2)
+        if (capacidade < 2)
             throw new Exception("A capacidade do evento tem que ser maior que 2!!");
 
-        repositorio.adicionarEvento(data, descricao, capacidade, preco);
+        Evento novoEvento = new Evento(repositorio.gerarId(), data, descricao, capacidade, preco);
+        repositorio.adicionar(novoEvento);
+    }
+
+    public static void criarParticipante(String cpf, String nascimento) throws Exception {
+        if (repositorio.localizarParticipante(cpf) != null)
+            throw new Exception("Já existe um participante com este cpf. Informe outro.");
+
+        Participante novoParticipante = new Participante(cpf, nascimento);
+        repositorio.adicionar(novoParticipante);
+    }
+
+    public static void criarConvidado(String cpf, String nascimento, String empresa) throws Exception {
+        if (repositorio.localizarParticipante(cpf) != null)
+            throw new Exception("Já existe um convidado com este cpf. Informe outro.");
+
+        Convidado novoConvidado = new Convidado(cpf, nascimento, empresa);
+        repositorio.adicionar(novoConvidado);
+    }
+
+    public static void criarIngresso(int id, String cpf, String telefone) throws Exception {
+        if (telefone.isEmpty()) throw new Exception("O telefone é obrigatório!!");
+
+        Evento evento = repositorio.localizarEvento(id);
+        Participante participante = repositorio.localizarParticipante(cpf);
+
+        if (evento == null)
+            throw new Exception("Evento inexistente. Por favor informe um evento existente.");
+
+        if (participante == null)
+            throw new Exception("Participante inexistente. Por favor informe um participante existente.");
+
+        if (evento.lotado()) throw new Exception("O evento já está cheio, tente outro evento.");
+
+        String codigo = id + "-" + cpf;
+        Ingresso novoIngresso = new Ingresso(codigo, telefone);
     }
 }
